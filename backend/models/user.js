3328d45d156ab1068c30f9ff177e8840671f7745
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-06-08 05:40:07 +0200

    Add user model

---
+++ backend/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32,
+        unique: true,
+        index: true,
+        lowercase: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        lowercase: true,
+        unique: true,
+    },
+    profile: {
+        type: String,
+        required: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    about: {
+        type: String
+    },
+    role: {
+        type: Number,
+        trim: true
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    resetPasswordLink: {
+        data: String,
+        default: ''
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
