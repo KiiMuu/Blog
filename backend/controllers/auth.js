2aa25f3ca156674a641a54a28fb83eb5d1c634b5
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-06-08 09:15:57 +0200

    Signup user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,39 @@
-exports.signup = (req, res, next) => {
-    const { name, email, password } = req.body;
+const User = require('../models/user');
+const shortId = require('shortid');
 
-    res.json({
-        user: {
+exports.signup = (req, res, next) => {
+    User.findOne({ email: req.body.email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is taken 🌚'
+            });
+        }
+
+        const { name, email, password } = req.body;
+        let username = shortId.generate();
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+        let newUser = new User({
             name,
             email,
-            password
-        }
+            password,
+            profile,
+            username
+        });
+
+        newUser.save((err, success) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+
+            // res.json({
+            //     user: success
+            // });
+            res.json({
+                message: 'SignUp success! Please SignIn.'
+            });
+        });
     });
 }
