102aaa65c4b0b92daaa0e0a6aa2e71f5047a34cf
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-06-08 08:46:29 +0200

    Add some methods and virtual fields to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -54,4 +54,37 @@ const userSchema = new Schema({
     timestamps: true
 });
 
+userSchema.virtual('password').set(function(password) {
+    // create a temp variable called _password
+    this._password = password;
+
+    // generate salt
+    this.salt = this.makeSalt();
+
+    // encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+}).get(function() {
+    return this._password;
+});
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return '';
+
+        try {
+            return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + ''
+    }
+}
+
 module.exports = mongoose.model('User', userSchema);
