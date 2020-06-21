77232bdd6da026bad6c91a99a0f98840f063a6c4
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-06-21 06:59:53 +0200

    Blog actions

--- frontend/components/crud/CreateBlog.js
+++ frontend/components/crud/CreateBlog.js
@@ -1,16 +1,25 @@
 import { useState, useEffect, Fragment } from 'react';
 import Link from 'next/link';
-import Router from 'next/router';
+import Router from 'next/router';   
+import dynamic from 'next/dynamic';
+import { withRouter } from 'next/router';
 import './Crud.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle, faTrash, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
-import { getCookie } from '../../actions/auth';
-import { createCategory, getCategories, removeCategory } from '../../actions/category';
+import {  } from '@fortawesome/free-solid-svg-icons';
+import { getCookie, isAuth } from '../../actions/auth';
+import { createBlog } from '../../actions/blog';
+import { getCategories } from '../../actions/category';
+import { getTags } from '../../actions/tag';
+const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
+import '../../node_modules/react-quill/dist/quill.snow.css';
 
-const CreateBlog = () => {
+const CreateBlog = ({ router }) => {
     return (
-        <div><h2>Create Blog Page</h2></div>
+        <div>
+            <h2>Create Blog Page</h2>
+            {JSON.stringify(router)} 
+        </div>
     )
 }
 
-export default CreateBlog;
+export default withRouter(CreateBlog);
