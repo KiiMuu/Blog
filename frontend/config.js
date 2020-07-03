2916472e3ffb37dbade92e6c79367a227ba8974a
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-07-03 09:16:54 +0200

    SEO head section for blogs

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,9 @@
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://bloggawy.com' : 'http://localhost:8000/api';
+export const API = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.API_PRODCUTION : publicRuntimeConfig.API_DEVELOPMENT;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
+
+export const DOMAIN = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.DOMAIN_PRODUCTION : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,12 +1,37 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Head from 'next/head';
+import { withRouter } from 'next/router';
 import './index.scss';
 import Layout from '../../components/layout/Layout';
 import { blogsWithCategoriesAndTags } from '../../actions/blog';
 import Card from '../../components/blog/Card';
 import Link from 'next/link';
+import { APP_NAME, API, DOMAIN, FB_APP_ID } from "../../config";
 
-const Blogs = ({ blogs, categories, tags, size }) => {
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+
+    const head = () => (
+        <Head>
+            <title>Programming blog | {APP_NAME}</title>
+            <meta 
+                name="description" 
+                content="Programming blogs - react node" 
+            />
+            <link rel="canonical" href={`${DOMAIN}/${router.pathname}`} />
+            <meta property="og:title" content={`Web dev tuts | ${APP_NAME}`} />
+            <meta 
+                property="og:description" 
+                content="Programming blogs - react node" 
+            />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}/${router.pathname}`} />
+            <meta property="og:site_name" content={`${APP_NAME}`} />
+            <meta property="og:image" content={`${DOMAIN}/public/img/seoblog.png`} />
+            <meta property="og:image:secure_url" content={`${DOMAIN}/public/img/seoblog.png`} />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+        </Head>
+    )
 
     const showAllCategories = () => {
         return categories.map((category, i) => (
@@ -35,36 +60,39 @@ const Blogs = ({ blogs, categories, tags, size }) => {
     }
 
     return (
-        <Layout>
-            <main>
-                <div className="uk-container">
-                    <div data-uk-grid>
-                        <div className="uk-width-1-1">
-                            <div className="categoreis_tags-content uk-margin-medium-top">
-                                <header>
-                                    <div className="heading">
-                                        <h2 className="uk-text-uppercase uk-text-center uk-margin-medium-bottom">Programming Blogs and Tutorials</h2>
-                                    </div>
-                                    <section>
-                                        <div className="cats">
-                                            {showAllCategories()}
+        <Fragment>
+            {head()}
+            <Layout>
+                <main>
+                    <div className="uk-container">
+                        <div data-uk-grid>
+                            <div className="uk-width-1-1">
+                                <div className="categoreis_tags-content uk-margin-medium-top">
+                                    <header>
+                                        <div className="heading">
+                                            <h2 className="uk-text-uppercase uk-text-center uk-margin-medium-bottom">Programming Blogs and Tutorials</h2>
                                         </div>
-                                        <div className="tags">
-                                            {showAllTags()}
-                                        </div>
-                                    </section>
-                                </header>
+                                        <section>
+                                            <div className="cats">
+                                                {showAllCategories()}
+                                            </div>
+                                            <div className="tags">
+                                                {showAllTags()}
+                                            </div>
+                                        </section>
+                                    </header>
+                                </div>
                             </div>
-                        </div>
-                        <div className="uk-width-1-1">
-                            <div className="uk-grid-small" data-uk-grid>
-                                {showAllBlogs()}
+                            <div className="uk-width-1-1">
+                                <div className="uk-grid-small" data-uk-grid>
+                                    {showAllBlogs()}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </main>
-        </Layout>
+                </main>
+            </Layout>
+        </Fragment>
     )
 }
 
@@ -83,4 +111,4 @@ Blogs.getInitialProps = () => {
     });
 }
 
-export default Blogs;
+export default withRouter(Blogs);
