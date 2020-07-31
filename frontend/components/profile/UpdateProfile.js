daccfefee8900e9c5209793233aadd4e61da2596
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-07-31 20:54:19 +0200

    update profile component

--- frontend/components/profile/UpdateProfile.js
+++ frontend/components/profile/UpdateProfile.js
@@ -1,6 +1,58 @@
+import { useState, Fragment, useEffect } from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+import { getCookie, isAuth } from '../../actions/auth';
+import { getProfile, updateProfile } from '../../actions/user';
+
 const UpdateProfile = () => {
+
+    const [values, setValues] = useState({
+        username: '',
+        name: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false,
+        loading: false,
+        photo: '',
+        userData: ''
+    });
+
+    const token = getCookie('token');
+    const { username, name, email, password, error, success, loading, photo, userData } = values;
+
+    const init = () => {
+        getProfile(token).then(data => {
+            if (data.error) {
+                setValues({
+                    ...values,
+                    error: data.error
+                });
+            } else {
+                setValues({
+                    ...values,
+                    username: data.username,
+                    name: data.name,
+                    email: data.email,
+                    about: data.about
+                });
+            }
+        });
+    }
+
+    useEffect(() => {
+        init();
+    }, []);
+
     return (
-        <p>Pro Up</p>
+        <Fragment>
+            <div className="uk-width-1-1">
+                <div className="user-img">image</div>
+                <div className="update-form">
+                    {JSON.stringify({username, email, name, password})}
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
