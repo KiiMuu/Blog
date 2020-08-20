732c075603cb1e586c235bd07c507b10e710f148
Karim MuHamad <karimmuhamadfci@gmail.com>
2020-08-20 10:37:27 +0200

    remove gmail password from smtp

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -40,7 +40,7 @@ exports.preSignup = (req, res, next) => {
                 secure: true,
                 auth: {
                     user: `${process.env.EMAIL_TO}`,
-                    pass: "Karim604050@FCIHGMAIL"
+                    pass: `${process.env.EMAIL_PASS}`
                 }
             }));
     
